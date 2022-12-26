fbb4617a43eff2aa4f312fdb7502c7f7bd5d3a15
Ronaldo J. Hernandez <ronaldjhdz@gmail.com>
2022-12-25 21:07:19 -0600

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //Ejercicio IF
+        int numeroIf = -10;
+
+        if(numeroIf > 0){
+            System.out.println("El numero es POSITIVO");
+        } else if (numeroIf < 0) {
+            System.out.println("El numero es NEGATIVO");
+        }else{
+            System.out.println("El numero es CERO");
+        }
+    }
+}
