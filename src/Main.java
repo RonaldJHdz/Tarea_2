08542dc1b7cc44c0826912a301cbce11131c7c09
Ronaldo J. Hernandez <ronaldjhdz@gmail.com>
2022-12-26 07:59:48 -0600

    agregar demas ejercicios

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,36 @@ public class Main {
         }else{
             System.out.println("El numero es CERO");
         }
+
+        //Ejercicio WHILE
+        int numeroWhile = 2;
+
+        while (numeroWhile < 3){
+            numeroWhile = numeroWhile + 1;
+            System.out.println(numeroWhile);
+        }
+
+        //Ejercicio DO WHILE
+        do {
+            numeroWhile = numeroWhile + 1;
+            System.out.println(numeroWhile);
+        }while (numeroWhile < 3);
+
+        //Ejercicio FOR
+
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor = numeroFor + 1){
+            System.out.println(numeroFor);
+        }
+
+        //Ejercicio SWITCH
+        String estacion = "VERANO";
+
+        switch (estacion) {
+            case "INVIERNO" -> System.out.println("Estas en INVIERNO");
+            case "VERANO" -> System.out.println("Estas en VERANO");
+            case "OTOÑO" -> System.out.println("Estas  en OTOÑO");
+            case "PRIMAVERA" -> System.out.println("Estas en PRIMAVERA");
+            default -> System.out.println("la opcion no es una estacion del año");
+        }
     }
 }
